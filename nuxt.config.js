a0d8abe43bc8cb7fe83df3b072905fa7daffb28e
vpp <vishnupp.habteq@gmail.com>
2026-08-16 16:00:14 +0530

    youtube change

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   css: ["normalize.css/normalize.css", "milligram/dist/milligram.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/vue-youtube-player.js', '~plugins/vue-gallery.client.js'],
+  plugins: ['~/plugins/vue-youtube-player.client.js', '~plugins/vue-gallery.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
